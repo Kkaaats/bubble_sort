487577ebea11cb0b962c5724c4d276c70cbd924c
Kkaaats <ekaterinaharris03@gmail.com>
2022-06-28 20:11:03 +0300

    Bubble Sort

---
+++ BubbleSort/Bubbles.cpp
@@ -0,0 +1,48 @@
+﻿// Bubbles.cpp  
+#include <iostream>
+using namespace std;
+
+class BubbleSort {
+public:
+	void SortArr();
+};
+
+void BubbleSort::SortArr() {
+	int n;
+	cout << "Enter the dimension of the array" << endl;
+	cin >> n;
+	cout << "\n";
+	if (n <= 0) {
+		cout << "Size must be positive integer";
+	}
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand();
+		cout << arr[i] << "\t";
+	}
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int a = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = a;
+			}
+		}
+	}
+	cout << "\n\n Result of sorting the array: \n";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\t";
+	}
+	delete[] arr;
+}
+
+
+int main()
+{
+	BubbleSort Numbers;
+	Numbers.SortArr();
+	return 0;
+}
+
+
+
---
+++ TestBubbleSort/UnitTest2.cpp
@@ -0,0 +1,89 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Bubbles\Bubbles\Bubbles.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		TEST_METHOD(test1)
+		{
+			BubbleSort Arr;
+			int n = 0;
+			int expected[] = { 1, 3, 5, 7, 9 };
+			int t[] = { 3, 9, 5, 1,7 };
+			Arr.SortArr();
+			for (int i = 0; i < 5; i++)
+			{
+				if (expected[i] == t[i])
+				{
+					n += 1;
+				}
+			}
+			if (n == 5)
+			{
+				Logger::WriteMessage("Successful sorting");
+			}
+		}
+		TEST_METHOD(test2)
+		{
+			BubbleSort Arr;
+			int n = 0;
+			int expected[] = { -3, -1, 5, 7, 9 };
+			int t[] = { -3, 9, 5, -1,7 };
+			Arr.SortArr();
+			for (int i = 0; i < 5; i++)
+			{
+				if (expected[i] == t[i])
+				{
+					n += 1;
+				}
+			}
+			if (n == 5)
+			{
+				Logger::WriteMessage("Successful sorting");
+			}
+		}
+		TEST_METHOD(test3)
+		{
+			BubbleSort Arr;
+			int n = 0;
+			int expected[] = { 0 };
+			int t[] = { 0 };
+			Arr.SortArr();
+			for (int i = 0; i < 5; i++)
+			{
+				if (expected[i] == t[i])
+				{
+					n += 1;
+				}
+			}
+			if (n == 5)
+			{
+				Logger::WriteMessage("Successful sorting");
+			}
+		}
+		TEST_METHOD(test4)
+		{
+			BubbleSort Arr;
+			int n = 0;
+			int expected[] = {  1, 1, 1 };
+			int t[] = { 1, 1, 1 };
+			Arr.SortArr();
+			for (int i = 0; i < 5; i++)
+			{
+				if (expected[i] == t[i])
+				{
+					n += 1;
+				}
+			}
+			if (n == 5)
+			{
+				Logger::WriteMessage("Successful sorting");
+			}
+		}
+	};
+}
